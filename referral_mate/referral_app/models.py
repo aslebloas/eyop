41b1226ad3ee958e0ac290bb3af5f58313d948f4
Anne Sophie <aslebloas@gmail.com>
2019-03-11 14:23:05 -0700

    Add layouts to templates + create new fields for profile model

--- referral_mate/referral_app/models.py
+++ referral_mate/referral_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(null=True, blank=True, max_length=32)
+    last_name = models.CharField(null=True, blank=True, max_length=32)
+    city = models.CharField(null=True, blank=True, max_length=32)
+
+    def __str__(self):
+        return f'{self.user.username}'
