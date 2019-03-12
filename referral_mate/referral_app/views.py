75846a17272dd0f644c46a57bf127644a185b4a8
Anne Sophie <aslebloas@gmail.com>
2019-03-11 19:42:20 -0700

    Add views for CRUD codes

--- referral_mate/referral_app/views.py
+++ referral_mate/referral_app/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from .models import Code
+
 
 # Create your views here.
 def home(request):
@@ -14,14 +16,14 @@ def home(request):
 
 def register(request):
     if request.method == 'POST':
-      form = RegisterForm(request.POST)
-      if form.is_valid():
-        form.save()
-        username = form.cleaned_data['username']
-        messages.success(request, f'Account created for {username}')
-        return redirect('login')
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            messages.success(request, f'Account created for {username}')
+            return redirect('login')
     if request.method == 'GET':
-      form = RegisterForm()
+        form = RegisterForm()
     return render(request, 'referral_app/register.html', {'form': form})
 
 
@@ -32,3 +34,21 @@ def profile(request):
 
 class CodesList(ListView):
     model = Code
+
+
+class CodeCreate(LoginRequiredMixin, CreateView):
+    model = Code
+    fields = ['code', 'amount', 'UOM', 'criteria']
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class CodeUpdate(LoginRequiredMixin, UpdateView):
+    model = Code
+
+
+class CodeDelete(LoginRequiredMixin, DeleteView):
+    model = Code
+    success_url = 'codes-list'
