e5f2a6972602441538339029528c6f14ef186585
Anne Sophie <aslebloas@gmail.com>
2019-03-11 20:14:14 -0700

    Update nav bar with user login status

--- referral_mate/referral_app/admin.py
+++ referral_mate/referral_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Profile, Code
+from .models import Profile, Code, Brand
 
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Code)
+admin.site.register(Brand)
