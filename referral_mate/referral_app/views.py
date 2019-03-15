5952c5885731ee43300725fce9938d27b2bbc6a3
leo byeon <ilbyeon@gmail.com>
2019-03-14 17:20:13 -0700

    Added basic facebook login and logout

--- referral_mate/referral_app/views.py
+++ referral_mate/referral_app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm, AdminPasswordChangeForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from .models import Code, Relationship
+from social_django.models import UserSocialAuth
 
 
 def register(request):
@@ -38,6 +41,66 @@ def home(request):
         from_person=request.user.profile)
     return render(
         request, 'referral_app/code_list.html', {'codes': codes, 'relationships': relationships})
+
+""" Social OAuth settings and password change begin """
+
+@login_required
+def settings(request):
+    user = request.user
+
+    try:
+        facebook_login = user.social_auth.get(provider='facebook')
+    except UserSocialAuth.DoesNotExist:
+        facebook_login = None
+
+    can_disconnect = (user.social_auth.count() >
+                      1 or user.has_usable_password())
+
+    return render(request, 'referral_app/settings.html', {
+        'facebook_login': facebook_login,
+        'can_disconnect': can_disconnect
+    })
+
+
+@login_required
+def password(request):
+    if request.user.has_usable_password():
+        PasswordForm = PasswordChangeForm
+    else:
+        PasswordForm = AdminPasswordChangeForm
+
+    if request.method == 'POST':
+        form = PasswordForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            messages.success(
+                request, 'Your password was successfully updated!')
+            return redirect('password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordForm(request.user)
+    return render(request, 'referral_app/password.html', {'form': form})
+
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(
+                request, 'Your password was successfully updated!')
+            return redirect('change_password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'referral_app/password.html', {'form': form})
+
+
+""" Social OAuth settings and password change end """
 
 
 class CodeCreate(LoginRequiredMixin, CreateView):
