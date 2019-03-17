643274926bcc73ec362bc342060fa0f23743ecd1
Anne Sophie <aslebloas@gmail.com>
2019-03-17 15:57:58 -0700

    Add invitation to admin panel

--- referral_mate/referral_app/admin.py
+++ referral_mate/referral_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Profile, Code, Brand, Relationship
+from .models import Profile, Code, Brand, Relationship, Invitation
 
 
 # Register your models here.
@@ -7,3 +7,4 @@ admin.site.register(Profile)
 admin.site.register(Code)
 admin.site.register(Brand)
 admin.site.register(Relationship)
+admin.site.register(Invitation)
